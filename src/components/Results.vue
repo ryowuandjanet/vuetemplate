363bcf132fd356976ba3eb07f12d04c59fcc4ff2
Ubuntu <ubuntu@ip-172-31-1-139.ap-southeast-1.compute.internal>
2021-01-25 06:53:40 +0000

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }}</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return{
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 500 ) {
+      this.rank = '忍者等級'
+    } else if (this.score < 800 ) {
+      this.rank = '常人等級'
+    } else {
+      this.rank = '手殘等級'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
